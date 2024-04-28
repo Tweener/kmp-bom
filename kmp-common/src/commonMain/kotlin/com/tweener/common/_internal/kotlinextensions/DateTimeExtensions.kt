8cb265be54c837c16b954e1a49bd89f6461f56bf
Vivien Mahé <vivouneuh@gmail.com>
2024-04-28 19:23:44 +0200

    Creates new function to transform LocalDate to/from epoch milliseconds

--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DateTimeExtensions.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DateTimeExtensions.kt
@@ -1,9 +1,12 @@
 package com.tweener.common._internal.kotlinextensions
 
 import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
 
 /**
@@ -18,3 +21,10 @@ fun LocalDate.Companion.now(timeZone: TimeZone = TimeZone.currentSystemDefault()
 fun LocalDate.isAfter(other: LocalDate) = this > other
 fun LocalDate.isBefore(other: LocalDate) = this < other
 fun LocalDate.isSameDate(other: LocalDate) = this == other
+
+fun LocalDate.toEpochMilliseconds(timeZone: TimeZone = TimeZone.currentSystemDefault()): Long {
+    val timeNow = LocalDateTime.now(timeZone = timeZone)
+    return this.atTime(hour = timeNow.hour, minute = timeNow.minute).toInstant(timeZone = timeZone).toEpochMilliseconds()
+}
+
+fun Long.fromEpochMilliseconds(timeZone: TimeZone = TimeZone.currentSystemDefault()) = Instant.fromEpochMilliseconds(this).toLocalDateTime(timeZone)
