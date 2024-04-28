b4ebe42a89d687ca13b229e21e9dc493a96a8454
Vivien Mahé <vivouneuh@gmail.com>
2024-04-28 19:25:21 +0200

    Adds a function extension to round a Double with a number of decimal digits

--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DoubleExtension.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DoubleExtension.kt
@@ -2,6 +2,7 @@ package com.tweener.common._internal.kotlinextensions
 
 import com.tweener.common._internal.utils.Degrees
 import com.tweener.common._internal.utils.Radians
+import kotlin.math.pow
 
 /**
  * @author Vivien Mahe
@@ -17,3 +18,9 @@ inline val Double.degrees: Degrees get() = Degrees(value = this)
  * Returns a [Radians] equals to this [Double] value.
  */
 inline val Double.radians: Radians get() = Radians(value = this)
+
+/**
+ * Rounds this [Double] with [decimalPrecision] as the number of decimal digits.
+ */
+fun Double.round(decimalPrecision: Int): Double =
+    kotlin.math.round(this * 10.0.pow(decimalPrecision)) / 10.0.pow(decimalPrecision)
