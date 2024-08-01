1bf5eb318aac44e0d5c93b16a14a5b43657ea9d5
Vivien Mahé <vivouneuh@gmail.com>
2024-08-01 12:27:36 +0200

    Creates Firestore extensions functions to handle LocaleDate(Time) mapping to Timestamp

--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DateTimeExtensions.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DateTimeExtensions.kt
@@ -15,25 +15,87 @@ import kotlinx.datetime.toLocalDateTime
  * @since 05/10/2023
  */
 
+/**
+ * Returns the current LocalDateTime in the specified time zone.
+ *
+ * @param timeZone The time zone to use. Defaults to the system's current time zone.
+ * @return The current LocalDateTime in the specified time zone.
+ */
 fun LocalDateTime.Companion.now(timeZone: TimeZone = TimeZone.currentSystemDefault()) = Clock.System.now().toLocalDateTime(timeZone)
 
+/**
+ * Returns the current LocalDate in the specified time zone.
+ *
+ * @param timeZone The time zone to use. Defaults to the system's current time zone.
+ * @return The current LocalDate in the specified time zone.
+ */
 fun LocalDate.Companion.now(timeZone: TimeZone = TimeZone.currentSystemDefault()) = LocalDateTime.now(timeZone).date
 
+/**
+ * Returns the current LocalTime in the specified time zone.
+ *
+ * @param timeZone The time zone to use. Defaults to the system's current time zone.
+ * @return The current LocalTime in the specified time zone.
+ */
 fun LocalTime.Companion.now(timeZone: TimeZone = TimeZone.currentSystemDefault()) = LocalDateTime.now(timeZone).time
 
+/**
+ * Converts this LocalDate to a LocalDateTime at the current time in the specified time zone.
+ *
+ * @param timeZone The time zone to use. Defaults to the system's current time zone.
+ * @return The LocalDateTime corresponding to this LocalDate at the current time.
+ */
 fun LocalDate.toLocalDateTime(timeZone: TimeZone = TimeZone.currentSystemDefault()) = this.atTime(time = LocalTime.now(timeZone))
 
+/**
+ * Checks if this LocalDate is after the specified LocalDate.
+ *
+ * @param other The other LocalDate to compare to.
+ * @return True if this LocalDate is after the specified LocalDate, false otherwise.
+ */
 fun LocalDate.isAfter(other: LocalDate) = this > other
+
+/**
+ * Checks if this LocalDate is before the specified LocalDate.
+ *
+ * @param other The other LocalDate to compare to.
+ * @return True if this LocalDate is before the specified LocalDate, false otherwise.
+ */
 fun LocalDate.isBefore(other: LocalDate) = this < other
+
+/**
+ * Checks if this LocalDate is the same as the specified LocalDate.
+ *
+ * @param other The other LocalDate to compare to.
+ * @return True if this LocalDate is the same as the specified LocalDate, false otherwise.
+ */
 fun LocalDate.isSameDate(other: LocalDate) = this == other
 
+/**
+ * Converts this LocalDate to the number of milliseconds since the epoch at the current time in the specified time zone.
+ *
+ * @param timeZone The time zone to use. Defaults to the system's current time zone.
+ * @return The number of milliseconds since the epoch for this LocalDate at the current time.
+ */
 fun LocalDate.toEpochMilliseconds(timeZone: TimeZone = TimeZone.currentSystemDefault()): Long {
     val timeNow = LocalDateTime.now(timeZone = timeZone)
     return this.atTime(hour = timeNow.hour, minute = timeNow.minute).toInstant(timeZone = timeZone).toEpochMilliseconds()
 }
 
+/**
+ * Converts this LocalDateTime to the number of milliseconds since the epoch in the specified time zone.
+ *
+ * @param timeZone The time zone to use. Defaults to the system's current time zone.
+ * @return The number of milliseconds since the epoch for this LocalDateTime.
+ */
 fun LocalDateTime.toEpochMilliseconds(timeZone: TimeZone = TimeZone.currentSystemDefault()): Long {
     return this.toInstant(timeZone = timeZone).toEpochMilliseconds()
 }
 
+/**
+ * Converts the given number of milliseconds since the epoch to a LocalDateTime in the specified time zone.
+ *
+ * @param timeZone The time zone to use. Defaults to the system's current time zone.
+ * @return The LocalDateTime corresponding to the given number of milliseconds since the epoch.
+ */
 fun Long.fromEpochMilliseconds(timeZone: TimeZone = TimeZone.currentSystemDefault()) = Instant.fromEpochMilliseconds(this).toLocalDateTime(timeZone)
---
+++ kmp-firebase/src/commonMain/kotlin/com/tweener/firebase/firestore/FirestoreExtensions.kt
@@ -0,0 +1,42 @@
+package com.tweener.firebase.firestore
+
+import com.tweener.common._internal.kotlinextensions.fromEpochMilliseconds
+import com.tweener.common._internal.kotlinextensions.toEpochMilliseconds
+import dev.gitlive.firebase.firestore.Timestamp
+import dev.gitlive.firebase.firestore.fromMilliseconds
+import dev.gitlive.firebase.firestore.toMilliseconds
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+
+/**
+ * @author Vivien Mahe
+ * @since 31/07/2024
+ */
+
+/**
+ * Converts this LocalDate to a Firestore Timestamp.
+ *
+ * @return The Firestore Timestamp corresponding to this LocalDate.
+ */
+fun LocalDate.toTimestamp() = Timestamp.fromMilliseconds(this.toEpochMilliseconds().toDouble())
+
+/**
+ * Converts this LocalDateTime to a Firestore Timestamp.
+ *
+ * @return The Firestore Timestamp corresponding to this LocalDateTime.
+ */
+fun LocalDateTime.toTimestamp() = Timestamp.fromMilliseconds(this.toEpochMilliseconds().toDouble())
+
+/**
+ * Converts this Firestore Timestamp to a LocalDateTime.
+ *
+ * @return The LocalDateTime corresponding to this Firestore Timestamp.
+ */
+fun Timestamp.toLocalDateTime() = this.toMilliseconds().toLong().fromEpochMilliseconds()
+
+/**
+ * Converts this Firestore Timestamp to a LocalDate.
+ *
+ * @return The LocalDate corresponding to this Firestore Timestamp.
+ */
+fun Timestamp.toLocalDate() = this.toLocalDateTime().date
