88fb9458af473a27726fa4a9b79b689efa10d680
Vivien Mahé <vivouneuh@gmail.com>
2024-08-01 12:31:30 +0200

    Renames file to the right platform name

--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/Platform.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/Platform.kt
@@ -1,6 +1,10 @@
 package com.tweener.common._internal
 
 /**
+ * Enum class representing the different platforms.
+ *
+ * Currently, it includes options for Android and iOS, but more platforms might be added in the future.
+ *
  * @author Vivien Mahe
  * @since 24/07/2024
  */
@@ -9,4 +13,11 @@ enum class Platform {
     IOS
 }
 
+/**
+ * The current platform on which the code is running.
+ *
+ * This is an expected property that should be implemented in the respective platform-specific module.
+ * It returns a value from the [Platform] enum indicating the platform on which the code is currently running.
+ * Currently, it includes options for Android and iOS, but more platforms might be added in the future.
+ */
 expect val currentPlatform: Platform
