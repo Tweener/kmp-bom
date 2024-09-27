28185e92c8db4d83b2f2445e82a1b45fff91aff4
Vivien Mahé <vivouneuh@gmail.com>
2024-09-27 14:01:39 +0200

    Update BOM version to 2.0.4

--- buildSrc/src/main/kotlin/BomConfiguration.kt
+++ buildSrc/src/main/kotlin/BomConfiguration.kt
@@ -7,7 +7,7 @@ object BomConfiguration {
 
     // BoM configuration
     const val artifactId = "kmp-bom"
-    const val version = "2.0.5"
+    const val version = "2.0.4"
     const val compileSDK = 34
     const val minSDK = 24
 
