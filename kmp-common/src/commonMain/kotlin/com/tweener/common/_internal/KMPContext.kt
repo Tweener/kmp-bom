7acebb5083b0b7dbd3c22f2beb0676fb0157d3ad
Vivien Mahé <vivouneuh@gmail.com>
2024-09-27 13:25:41 +0200

    Update some dependencies versions
    Kotlin 2.0.20
    Gradle 8.5.2
    KotlinXDatetime 0/6.1
    Realm 2.3.0
    SerializationJson 1.7.2

--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/KMPContext.kt
+++
@@ -1,8 +0,0 @@
-package com.tweener.common._internal
-
-/**
- * @author Vivien Mahe
- * @since 10/02/2024
- */
-
-expect class KMPContext
--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/utils/TrigonometryUtils.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/utils/TrigonometryUtils.kt
@@ -11,45 +11,45 @@ import kotlin.math.PI
 @JvmInline
 value class Degrees(val value: Double) : Comparable<Degrees> {
 
-    inline operator fun plus(other: Degrees) = Degrees(value = this.value + other.value)
+    operator fun plus(other: Degrees) = Degrees(value = this.value + other.value)
 
-    inline operator fun plus(other: Int) = Degrees(value = this.value + other)
+    operator fun plus(other: Int) = Degrees(value = this.value + other)
 
-    inline operator fun plus(other: Float) = Degrees(value = this.value + other)
+    operator fun plus(other: Float) = Degrees(value = this.value + other)
 
-    inline operator fun plus(other: Double) = Degrees(value = this.value + other)
+    operator fun plus(other: Double) = Degrees(value = this.value + other)
 
-    inline operator fun minus(other: Degrees) = Degrees(value = this.value - other.value)
+    operator fun minus(other: Degrees) = Degrees(value = this.value - other.value)
 
-    inline operator fun minus(other: Int) = Degrees(value = this.value - other)
+    operator fun minus(other: Int) = Degrees(value = this.value - other)
 
-    inline operator fun minus(other: Float) = Degrees(value = this.value - other)
+    operator fun minus(other: Float) = Degrees(value = this.value - other)
 
-    inline operator fun minus(other: Double) = Degrees(value = this.value - other)
+    operator fun minus(other: Double) = Degrees(value = this.value - other)
 
-    inline operator fun div(other: Degrees): Degrees = Degrees(value = value / other.value)
+    operator fun div(other: Degrees): Degrees = Degrees(value = value / other.value)
 
-    inline operator fun div(other: Int): Degrees = Degrees(value = value / other)
+    operator fun div(other: Int): Degrees = Degrees(value = value / other)
 
-    inline operator fun div(other: Float): Degrees = Degrees(value = value / other)
+    operator fun div(other: Float): Degrees = Degrees(value = value / other)
 
-    inline operator fun div(other: Double): Degrees = Degrees(value = value / other)
+    operator fun div(other: Double): Degrees = Degrees(value = value / other)
 
-    inline operator fun times(other: Degrees): Degrees = Degrees(value = value * other.value)
+    operator fun times(other: Degrees): Degrees = Degrees(value = value * other.value)
 
-    inline operator fun times(other: Int): Degrees = Degrees(value = value * other)
+    operator fun times(other: Int): Degrees = Degrees(value = value * other)
 
-    inline operator fun times(other: Float): Degrees = Degrees(value = value * other)
+    operator fun times(other: Float): Degrees = Degrees(value = value * other)
 
-    inline operator fun times(other: Double): Degrees = Degrees(value = value * other)
+    operator fun times(other: Double): Degrees = Degrees(value = value * other)
 
     override fun compareTo(other: Degrees): Int = value.compareTo(other.value)
 
-    inline operator fun compareTo(other: Int): Int = value.compareTo(other)
+    operator fun compareTo(other: Int): Int = value.compareTo(other)
 
-    inline operator fun compareTo(other: Float): Int = value.compareTo(other)
+    operator fun compareTo(other: Float): Int = value.compareTo(other)
 
-    inline operator fun compareTo(other: Double): Int = value.compareTo(other)
+    operator fun compareTo(other: Double): Int = value.compareTo(other)
 
     /**
      * Converts this [Degrees] to a [Radians].
@@ -60,45 +60,45 @@ value class Degrees(val value: Double) : Comparable<Degrees> {
 @JvmInline
 value class Radians(val value: Double) : Comparable<Radians> {
 
-    inline operator fun plus(other: Radians) = Radians(value = this.value + other.value)
+    operator fun plus(other: Radians) = Radians(value = this.value + other.value)
 
-    inline operator fun plus(other: Int) = Radians(value = this.value + other)
+    operator fun plus(other: Int) = Radians(value = this.value + other)
 
-    inline operator fun plus(other: Float) = Radians(value = this.value + other)
+    operator fun plus(other: Float) = Radians(value = this.value + other)
 
-    inline operator fun plus(other: Double) = Radians(value = this.value + other)
+    operator fun plus(other: Double) = Radians(value = this.value + other)
 
-    inline operator fun minus(other: Radians) = Radians(value = this.value - other.value)
+    operator fun minus(other: Radians) = Radians(value = this.value - other.value)
 
-    inline operator fun minus(other: Int) = Radians(value = this.value + other)
+    operator fun minus(other: Int) = Radians(value = this.value + other)
 
-    inline operator fun minus(other: Float) = Radians(value = this.value + other)
+    operator fun minus(other: Float) = Radians(value = this.value + other)
 
-    inline operator fun minus(other: Double) = Radians(value = this.value + other)
+    operator fun minus(other: Double) = Radians(value = this.value + other)
 
-    inline operator fun div(other: Radians): Radians = Radians(value = value / other.value)
+    operator fun div(other: Radians): Radians = Radians(value = value / other.value)
 
-    inline operator fun div(other: Int) = Radians(value = this.value + other)
+    operator fun div(other: Int) = Radians(value = this.value + other)
 
-    inline operator fun div(other: Float) = Radians(value = this.value + other)
+    operator fun div(other: Float) = Radians(value = this.value + other)
 
-    inline operator fun div(other: Double): Radians = Radians(value = value / other)
+    operator fun div(other: Double): Radians = Radians(value = value / other)
 
-    inline operator fun times(other: Radians): Radians = Radians(value = value * other.value)
+    operator fun times(other: Radians): Radians = Radians(value = value * other.value)
 
-    inline operator fun times(other: Int) = Radians(value = this.value + other)
+    operator fun times(other: Int) = Radians(value = this.value + other)
 
-    inline operator fun times(other: Float) = Radians(value = this.value + other)
+    operator fun times(other: Float) = Radians(value = this.value + other)
 
-    inline operator fun times(other: Double): Radians = Radians(value = value * other)
+    operator fun times(other: Double): Radians = Radians(value = value * other)
 
     override fun compareTo(other: Radians): Int = value.compareTo(other.value)
 
-    inline operator fun compareTo(other: Int): Int = value.compareTo(other)
+    operator fun compareTo(other: Int): Int = value.compareTo(other)
 
-    inline operator fun compareTo(other: Float): Int = value.compareTo(other)
+    operator fun compareTo(other: Float): Int = value.compareTo(other)
 
-    inline operator fun compareTo(other: Double): Int = value.compareTo(other)
+    operator fun compareTo(other: Double): Int = value.compareTo(other)
 
     /**
      * Converts this [Radians] to a [Degrees].
