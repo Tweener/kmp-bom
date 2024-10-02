a3651cf1d855272e9a83131559f7821871943192
Vivien Mahé <vivouneuh@gmail.com>
2024-10-02 11:24:46 +0200

    Common - Add extension function safeDiv() to safely handle division by 0

--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DoubleExtensions.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/DoubleExtensions.kt
@@ -34,3 +34,22 @@ fun Double.round(decimalPrecision: Int): Double =
  * ```
  */
 fun Double.applyPercent(percent: Float) = this + this * percent / 100
+
+/**
+ * Safely divides the current `Double` by the provided `Number` divisor.
+ *
+ * If the divisor is zero, the function returns the specified `resultIfZero`, or `0.0` by default, to avoid division by zero errors.
+ * Otherwise, it performs the division and returns the result.
+ *
+ * @param divisor The divisor as a `Number`. The function will convert this to a `Double`.
+ * @param resultIfZero The value to return if the divisor is zero. Defaults to `0.0`.
+ * @return The result of dividing the current `Double` by the `divisor`, or the specified `resultIfZero` (default is `0.0`) if the divisor is zero.
+ *
+ * Example usage:
+ * ```
+ * val result = 10.0.safeDiv(2) // result will be 5.0
+ * val zeroDiv = 10.0.safeDiv(0) // zeroDiv will be 0.0
+ * val customDiv = 10.0.safeDiv(0, -1.0) // customDiv will be -1.0
+ * ```
+ */
+fun Double.safeDiv(divisor: Number, resultIfZero: Double = 0.0): Double = if (divisor.toDouble() == 0.0) resultIfZero else this / divisor.toDouble()
--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/FloatExtensions.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/FloatExtensions.kt
@@ -27,3 +27,22 @@ inline val Float.radians: Radians get() = Radians(value = this.toDouble())
  * ```
  */
 fun Float.applyPercent(percent: Float) = this + this * percent / 100
+
+/**
+ * Safely divides the current `Float` by the provided `Number` divisor.
+ *
+ * If the divisor is zero, the function returns the specified `resultIfZero`, or `0f` by default, to avoid division by zero errors.
+ * Otherwise, it performs the division and returns the result.
+ *
+ * @param divisor The divisor as a `Number`. The function will convert this to a `Float`.
+ * @param resultIfZero The value to return if the divisor is zero. Defaults to `0f`.
+ * @return The result of dividing the current `Float` by the `divisor`, or the specified `resultIfZero` (default is `0f`) if the divisor is zero.
+ *
+ * Example usage:
+ * ```
+ * val result = 10.0f.safeDiv(2) // result will be 5.0f
+ * val zeroDiv = 10.0f.safeDiv(0) // zeroDiv will be 0f
+ * val customDiv = 10.0f.safeDiv(0, -1.0f) // customDiv will be -1.0f
+ * ```
+ */
+fun Float.safeDiv(divisor: Number, resultIfZero: Float = 0f): Float = if (divisor.toFloat() == 0f) resultIfZero else this / divisor.toFloat()
