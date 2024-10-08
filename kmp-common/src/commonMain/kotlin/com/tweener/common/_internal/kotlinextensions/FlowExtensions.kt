69dedfba36ff09b8a774fd1a6eb975fb3d8a9a62
Vivien Mahé <vivouneuh@gmail.com>
2024-10-08 06:08:55 +0200

    Common - Flow can now combine up to 6 flows.

--- kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/FlowExtensions.kt
+++ kmp-common/src/commonMain/kotlin/com/tweener/common/_internal/kotlinextensions/FlowExtensions.kt
@@ -94,3 +94,27 @@ fun CoroutineScope.onInit(block: () -> Unit) = flow {
     block()
     emit(Unit)
 }.stateIn(scope = this, started = SharingStarted.Eagerly, initialValue = Unit)
+
+/**
+ * Returns a Flow whose values are generated with transform function by combining the most recently emitted values by each flow.
+ * This is the same as [kotlinx.coroutines.flow.combine] with 6 parameters.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6, R> combine(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    transform: suspend (T1, T2, T3, T4, T5, T6) -> R
+): Flow<R> = kotlinx.coroutines.flow.combine(flow, flow2, flow3, flow4, flow5, flow6) { args: Array<*> ->
+    transform(
+        args[0] as T1,
+        args[1] as T2,
+        args[2] as T3,
+        args[3] as T4,
+        args[4] as T5,
+        args[5] as T6,
+    )
+}
