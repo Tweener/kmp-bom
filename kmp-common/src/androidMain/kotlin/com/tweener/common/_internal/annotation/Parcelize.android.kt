8efcf69597768da8c5fdce7384b287ecb3a2137e
Vivien Mahé <vivouneuh@gmail.com>
2024-10-13 12:28:22 +0200

    Common - Removes Parcelize custom implementation since now available with Kotlin K2 compiler
    https://kotlinlang.org/docs/whatsnew20.html#compiler-plugins-support

--- kmp-common/src/androidMain/kotlin/com/tweener/common/_internal/annotation/Parcelize.android.kt
+++
@@ -1,8 +0,0 @@
-package com.tweener.common._internal.annotation
-
-/**
- * @author Vivien Mahe
- * @since 08/04/2024
- */
-
-actual typealias Parcelable = android.os.Parcelable
--- kmp-common/src/iosMain/kotlin/com/tweener/common/_internal/annotation/Parcelize.ios.kt
+++
@@ -1,8 +0,0 @@
-package com.tweener.common._internal.annotation
-
-/**
- * @author Vivien Mahe
- * @since 08/04/2024
- */
-
-actual interface Parcelable
