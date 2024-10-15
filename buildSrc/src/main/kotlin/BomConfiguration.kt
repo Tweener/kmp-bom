e616af14a92bcabd0f4530c30c03d19b2afbdc1d
Vivien Mahé <vivouneuh@gmail.com>
2024-10-15 17:11:41 +0200

    Update BoM version to 2.1.1 and Common to 1.1.1

--- buildSrc/src/main/kotlin/BomConfiguration.kt
+++ buildSrc/src/main/kotlin/BomConfiguration.kt
@@ -7,7 +7,7 @@ object BomConfiguration {
 
     // BoM configuration
     const val artifactId = "kmp-bom"
-    const val version = "2.1.0"
+    const val version = "2.1.1"
     const val compileSDK = 34
     const val minSDK = 24
 
@@ -15,7 +15,7 @@ object BomConfiguration {
 
         object Common {
             const val packageName = "com.tweener.common"
-            const val version = "1.1.0"
+            const val version = "1.1.1"
             const val namespace = "$packageName.android"
         }
 
