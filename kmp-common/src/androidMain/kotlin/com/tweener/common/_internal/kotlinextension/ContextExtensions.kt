8e44d78dee63cf3b424b520197b83d73d4471991
Vivien Mahé <vivouneuh@gmail.com>
2024-11-07 10:02:12 +0100

    Common - Create Context extension functions for Android target
    - getAlarmManager()
    - getNotificationManager()

---
+++ kmp-common/src/androidMain/kotlin/com/tweener/common/_internal/kotlinextension/ContextExtensions.kt
@@ -0,0 +1,14 @@
+package com.tweener.common._internal.kotlinextension
+
+import android.app.AlarmManager
+import android.app.NotificationManager
+import android.content.Context
+
+/**
+ * @author Vivien Mahe
+ * @since 06/11/2024
+ */
+
+fun Context.getAlarmManager(): AlarmManager? = getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+
+fun Context.getNotificationManager(): NotificationManager? = getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
